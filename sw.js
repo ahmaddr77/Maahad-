00e996dbd4579d5261dd7f7e0054100d8ad4252d
ahmaddr77 <ahmadalb20025@gmail.com>
2025-09-18 15:12:35 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker تم تثبيته");
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => new Response("أنت غير متصل الآن"))
-  );
-});
